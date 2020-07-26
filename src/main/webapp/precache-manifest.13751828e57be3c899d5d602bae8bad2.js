d981ade65cd108f2a28e2445c70efd0abad5f53e
Sushant Gupta <mya.sushant@yahoo.com>
2020-07-26 15:50:59 +0530

    Experiment with React CI/CD

---
+++ src/main/webapp/precache-manifest.13751828e57be3c899d5d602bae8bad2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b88336500b9cacc9b9c64e235e8f65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c2706a637c40ed55d12",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0cfc2a35b62c79f19efd",
+    "url": "/static/js/2.799f865b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.799f865b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2706a637c40ed55d12",
+    "url": "/static/js/main.7e146e4a.chunk.js"
+  },
+  {
+    "revision": "ee104fe0ae89069beb36",
+    "url": "/static/js/runtime-main.59d1ebf9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
