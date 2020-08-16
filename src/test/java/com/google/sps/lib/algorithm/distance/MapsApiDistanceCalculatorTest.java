b52c053fc0e0de9b6b6efa90dbdf40a6b148ab40
shradha <shradhakhapra1899@gmail.com>
2020-08-17 00:53:24 +0530

    Add test to verify request URL

--- src/test/java/com/google/sps/lib/algorithm/distance/MapsApiDistanceCalculatorTest.java
+++ src/test/java/com/google/sps/lib/algorithm/distance/MapsApiDistanceCalculatorTest.java
@@ -16,8 +16,11 @@ package com.google.sps.lib.algorithm.distance;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -73,5 +76,22 @@ public class MapsApiDistanceCalculatorTest {
                                                    new Coordinate(19.0760, 72.8777)));
     
     double distanceMatrix[][] = new MapsApiDistanceCalculator(mockUrlWrapper).findDistance(coordinates);
+  }
+
+  @Test
+  public void findDistance_successresponse_requestUrlAsExpected() throws Exception {
+    when(mockUrlWrapper.openConnection(any())).thenReturn(mockConnection);
+    when(mockConnection.getResponseCode()).thenReturn(200);
+    String apiResponse = "{ \"destination_addresses\" : [ \"Amar Jawan Jyoti Rd, India Gate, New Delhi, Delhi 110001, India\", \"24, Ramakanta Mistri Ln, Cossipore, Newland, College Square, Kolkata, West Bengal 700073, India\", \"1777, Lower Parel, Friends Colony, Kurla West, Kurla, Mumbai, Maharashtra 400070, India\" ], \"origin_addresses\" : [ \"Amar Jawan Jyoti Rd, India Gate, New Delhi, Delhi 110001, India\", \"24, Ramakanta Mistri Ln, Cossipore, Newland, College Square, Kolkata, West Bengal 700073, India\", \"1777, Lower Parel, Friends Colony, Kurla West, Kurla, Mumbai, Maharashtra 400070, India\" ], \"rows\" : [ { \"elements\" : [ { \"distance\" : { \"text\" : \"1 ft\", \"value\" : 0 }, \"duration\" : { \"text\" : \"1 min\", \"value\" : 0 }, \"status\" : \"OK\" }, { \"distance\" : { \"text\" : \"939 mi\", \"value\" : 1510850 }, \"duration\" : { \"text\" : \"1 day 2 hours\", \"value\" : 93607 }, \"status\" : \"OK\" }, { \"distance\" : { \"text\" : \"873 mi\", \"value\" : 1404782 }, \"duration\" : { \"text\" : \"23 hours 21 mins\", \"value\" : 84081 }, \"status\" : \"OK\" } ] }, { \"elements\" : [ { \"distance\" : { \"text\" : \"938 mi\", \"value\" : 1509173 }, \"duration\" : { \"text\" : \"1 day 2 hours\", \"value\" : 94436 }, \"status\" : \"OK\" }, { \"distance\" : { \"text\" : \"1 ft\", \"value\" : 0 }, \"duration\" : { \"text\" : \"1 min\", \"value\" : 0 }, \"status\" : \"OK\" }, { \"distance\" : { \"text\" : \"1,276 mi\", \"value\" : 2053949 }, \"duration\" : { \"text\" : \"1 day 17 hours\", \"value\" : 146217 }, \"status\" : \"OK\" } ] }, { \"elements\" : [ { \"distance\" : { \"text\" : \"876 mi\", \"value\" : 1409170 }, \"duration\" : { \"text\" : \"23 hours 25 mins\", \"value\" : 84308 }, \"status\" : \"OK\" }, { \"distance\" : { \"text\" : \"1,273 mi\", \"value\" : 2048291 }, \"duration\" : { \"text\" : \"1 day 16 hours\", \"value\" : 145790 }, \"status\" : \"OK\" }, { \"distance\" : { \"text\" : \"1 ft\", \"value\" : 0 }, \"duration\" : { \"text\" : \"1 min\", \"value\" : 0 }, \"status\" : \"OK\" } ] } ], \"status\" : \"OK\"}";
+    when(mockConnection.getInputStream()).thenReturn(new ByteArrayInputStream(apiResponse.getBytes("UTF-8")));
+    List<Coordinate> coordinates = new ArrayList<>(
+                                     Arrays.asList(new Coordinate(28.6129, 77.2295),
+                                                   new Coordinate(22.5726, 88.3639),
+                                                   new Coordinate(19.0760, 72.8777)));
+    String expectedRequestUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origin=28.6129,77.2295|22.5726,88.3639|19.0760,72.8777&destinations=28.6129,77.2295|22.5726,88.3639|19.0760,72.8777&key=__GOOGLE_MAPS_API_KEY__";
+    
+    double distanceMatrix[][] = new MapsApiDistanceCalculator(mockUrlWrapper).findDistance(coordinates);
+
+    verify(mockUrlWrapper.openConnection(expectedRequestUrl), times(1));
   }
 }
